b271dc6852f7b68ef25beca1891cc6c09c981577
Antoine D. Washington <dember224@gmail.com>
2017-07-06 00:55:21 -0400

    Ran a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620051650) do
+ActiveRecord::Schema.define(version: 20170705051013) do
 
   create_table "careers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "job"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "message"
+    t.integer "to"
+    t.integer "from"
   end
 
   create_table "pairs", force: :cascade do |t|
