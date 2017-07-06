7d69b2cbccb7d63dd3bac1df9a579ad1bd88752e
Antoine D. Washington <dember224@gmail.com>
2017-07-06 13:31:37 -0400

    Users can chat with mentors.

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,28 @@
+class MessageController < ApplicationController
+  def new
+  @message =  Message.new
+  @messages = Message.all
+  @user = User.all
+  end
+
+  def create
+  @message = Message.new(message_params)
+  end
+
+  def agreed
+    @pair = []
+    @choosen = []
+    for pair in Pair.all
+      if pair.user_id == current_user.id
+        @choosen.push(pair.mentee_id)
+      end
+    end
+
+    helper_method :agreed
+
+    private
+
+    def message_params
+      params.require(:message).permit(:from, :to, :message)
+    end
+end
