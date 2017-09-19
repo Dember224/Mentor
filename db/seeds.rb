c8e70d734e26a27ac24ed040edb730c6b1385d87
Antoine D. Washington <dember224@gmail.com>
2017-09-19 03:51:01 -0400

    updated Careers in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 User.create(name: "Antoine", email: "Dember224@gmail.com", expertise: "Expert", career: Career.first, description: "I'm a simple hobbyist trying to create the things that don't exist but should.", password: "dekaos")
 
-professions = ["Doctor", "Nurse", "Teacher", "Engineer", "Scientist", "Software Developer","Social Work", "Entrepreneur", "Acting/Directing", "Politics", "Law", "Marketing", "Athletics/Training"]
+professions = ["Medicine/Healthcare", "Education", "Engineering", "Scientist", "Software", "Social Work", "Entrepreneur", "Entertainment", "Politics", "Law", "Marketing", "Athletics/Training", "Public Policy", "Real Estate", "Finance", "Military", "Law Enforcement", "Culinary Arts", "Art", "Authorship/Publishing", "Journalism"]
 
 professions.each do |jobs|
   Career.create(job: jobs)
