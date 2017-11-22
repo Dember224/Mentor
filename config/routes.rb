47315318d3aed4ee986bcb7c0ae795184ef75639
Antoine D. Washington <dember224@gmail.com>
2017-11-22 01:44:44 -0500

    Making the theme coherent.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   delete 'logout' => 'sessions#destroy'
   get '/pairs' => 'pairs#new'
   post 'pairs' => 'pairs#create'
