a512b8d5857ce0975f98c6d1294c7cc78836402f
Antoine D. Washington <dember224@gmail.com>
2018-01-27 04:33:18 -0500

    Restyled the sign up page for a warmer more welcoming feel.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,23 +2,39 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: https://coffeescript.org/
 document.addEventListener("turbolinks:load",function() {
-  $('#identity').click(function() {
-    $('#expertise').css("display","inline-block")
+  $('#expertise').click(function() {
+    $('#description').css("display","inline-block")
     $('#two').css("display","none")
     });
 
-    $('#expertise').click(function() {
-      $('#description').css("display","inline-block")
+    $('#description').click(function() {
+      $('#career').css("display","inline-block")
       $('#three').css("display","none")
     });
 
-    $('#description').click(function() {
-      $('#career').css("display", "block")
+    $('#career').click(function() {
+      $('#user_info').css("display", "block")
       $('#four').css("display","none")
     });
 
-    $('#career').click(function() {
+    $('#user_info').click(function() {
       $('#submit').css("display", "block")
       $('#submit').scrollTop();
     });
+
+    if ($("input:checked")){
+      $('.experience_button').click(function() {
+        if (!$('#beginner').hasClass('selected') && !$('#expert').hasClass('selected')) {
+        $(this).toggleClass("selected")
+      } else if (!$(this).hasClass('selected')) {
+        $('#beginner').toggleClass("selected")
+        $('#expert').toggleClass("selected")
+      }
+      });
+    } else {
+      $('.experience_button').click(function() {
+        $(this).removeClass("selected")
+      });
+    }
+
   });
