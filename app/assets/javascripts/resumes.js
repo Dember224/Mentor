a7ad3018249141b3f287b2ee2ee6a592fdcf9d0f
Antoine D. Washington <dember224@gmail.com>
2018-02-22 02:19:31 -0500

    Resume fields collapsable. Shorter instructions. Title highliting.

--- app/assets/javascripts/resumes.js
+++ app/assets/javascripts/resumes.js
@@ -41,7 +41,12 @@ document.addEventListener("turbolinks:load",function() {
     $('#high_school').css('display', 'block')
     $('#title_high_school').css('display', 'inline')
     $('#hs').css('display', 'none')
+  });
 
+  $('#high_school_collapse').click(function(){
+    $('#high_school').css('display', 'none')
+    $('#title_high_school').css('display', 'none')
+    $('#hs').css('display', 'inline')
   });
 
   $('#ug').click(function(){
@@ -50,11 +55,23 @@ document.addEventListener("turbolinks:load",function() {
     $('#ug').css('display', 'none')
   });
 
+  $('#undergrad_collapse').click(function(){
+    $('#undergrad').css('display', 'none')
+    $('#title_undergrad').css('display', 'none')
+    $('#ug').css('display', 'inline')
+  });
+
   $('#advanced').click(function(){
     $('#advanced_degree').css('display', 'inline')
     $('#title_advanced_degree').css('display', 'inline')
     $('#advanced').css('display', 'none')
   });
+
+  $('#advanced_degree_collapse').click(function(){
+    $('#advanced_degree').css('display', 'none')
+    $('#title_advanced_degree').css('display', 'none')
+    $('#advanced').css('display', 'inline')
+  })
 
   $('#work').click(function(){
     $('#jobs').css('display', 'inline')
@@ -62,11 +79,23 @@ document.addEventListener("turbolinks:load",function() {
     $('#work').css('display', 'none')
   });
 
+  $('#collapse_work').click(function(){
+    $('#jobs').css('display', 'none')
+    $('#title_jobs').css('display', 'none')
+    $('#work').css('display', 'inline')
+  })
+
   $('#voluntary').click(function() {
     $('#volunteer').css('display', 'inline')
     $('#voluntary').css('display', 'none')
     $('#title_volunteer').css('display', 'inline')
   });
+
+  $('#collapse_voluntary').click(function(){
+    $('#volunteer').css('display', 'none')
+    $('#voluntary').css('display', 'inline')
+    $('#title_volunteer').css('display', 'none')
+  })
 
   $('#certified').click(function() {
     $('#certs').css('display', 'inline')
@@ -74,12 +103,22 @@ document.addEventListener("turbolinks:load",function() {
     $('#certified').css('display', 'none')
   });
 
+  $('#collapse_certs').click(function() {
+    $('#certs').css('display', 'none')
+    $('#title_cert').css('display', 'none')
+    $('#certified').css('display', 'inline')
+  })
+
   $('#hobbies').click(function() {
     $('#hobby').css('display', 'inline')
     $('#hobbies').css('display', 'none')
     $('#title_hobby').css('display', 'inline')
   });
 
-
+  $('#collapse_hobbies').click(function() {
+    $('#hobby').css('display', 'none')
+    $('#hobbies').css('display', 'inline')
+    $('#title_hobby').css('display', 'none')
+  })
 
 })
